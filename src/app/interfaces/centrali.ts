d5a515aa3583e96f366e2a947a019f00320e379a
luizfdr <luizefedrigorossi@gmail.com>
2024-06-06 17:09:12 +0200

    added centrali cards and grid layout to display those

---
+++ src/app/interfaces/centrali.ts
@@ -0,0 +1,7 @@
+export interface Centrali {
+    id: number;
+    name: string;
+    description: string;
+    percorso: number;
+    img: string;
+}
---
+++ src/app/interfaces/mock-centrali.ts
@@ -0,0 +1,27 @@
+import { Centrali } from './centrali'
+
+export const CENTRALIAGNO: Centrali[] = [
+  { id: 1, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 2, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 3, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 4, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 5, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 6, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 7, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 8, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 9, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 10, name: 'Agno', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+];
+
+export const CENTRALIASTI: Centrali[] = [
+  { id: 1, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 2, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 3, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 4, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 5, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 6, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 7, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 8, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 9, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+  { id: 10, name: 'Asti', description: 'test, description.', percorso: 1, img: "https://picsum.photos/200/100" },
+];
