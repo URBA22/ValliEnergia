e8df44458eee3d9624ea9b5d6f64e5f4b07616e6
luizfdr <luizefedrigorossi@gmail.com>
2024-06-06 18:02:06 +0200

    concatenation of two arrays to display all Centrali at once

--- src/app/components/centrali/centrali.component.ts
+++ src/app/components/centrali/centrali.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Centrali } from '../../interfaces/centrali';
 import { CENTRALIAGNO, CENTRALIASTI } from '../../interfaces/mock-centrali';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-centrali',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './centrali.component.html',
   styleUrl: './centrali.component.css'
 })
 export class CentraliComponent {
   centraliAgno = CENTRALIAGNO;
   centraliAsti = CENTRALIASTI;
+  centraliTutte = [...this.centraliAgno, ...this.centraliAsti];
 
 }
