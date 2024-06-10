6b12b4ddcf1feaf0ae77093cd82918bc2ecfc888
HyperDogeExe <dnltrct@gmail.com>
2024-06-10 15:08:42 +0200

    temp commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { CentraliComponent } from './components/centrali/centrali.component';
 import { PercorsiComponent } from './components/percorsi/percorsi.component';
-import { CentraleDetailComponent } from './components/centrale-detail/centrale-detail.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch: 'full'},
   {path: 'home', component: HomepageComponent},
   {path: 'centrali', component: CentraliComponent},
-  {path: 'centrali/:id', component: CentraleDetailComponent},
   {path: 'percorsi', component: PercorsiComponent}
 ];
 
