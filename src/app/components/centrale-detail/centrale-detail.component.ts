cde7543e2a57cb3861eacf3408f29255ad4b7022
HyperDogeExe <dnltrct@gmail.com>
2024-06-10 14:08:55 +0200

    commit to merge to fix branch

---
+++ src/app/components/centrale-detail/centrale-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-centrale-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './centrale-detail.component.html',
+  styleUrl: './centrale-detail.component.css'
+})
+export class CentraleDetailComponent {
+
+}
