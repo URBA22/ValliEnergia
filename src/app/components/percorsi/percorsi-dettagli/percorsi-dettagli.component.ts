7f457acb824aa5b32f7b366b5cf92023c1759216
StefanoDFranceschi <stedefranceschi06@gmail.com>
2024-06-14 09:27:34 +0200

    dasvidania

--- src/app/components/percorsi/percorsi-dettagli/percorsi-dettagli.component.ts
+++ src/app/components/percorsi/percorsi-dettagli/percorsi-dettagli.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Percorsi } from '../../../interfaces/percorsi';
+import { DataTransferServiceService } from '../../../services/data-transfer-service.service';
+import { Router } from '@angular/router';
+import { PercorsiService } from '../../../services/percorsi.service';
 
 @Component({
   selector: 'app-percorsi-dettagli',
-  standalone: true,
-  imports: [],
   templateUrl: './percorsi-dettagli.component.html',
-  styleUrl: './percorsi-dettagli.component.css'
+  styleUrls: ['./percorsi-dettagli.component.css']
 })
-export class PercorsiDettagliComponent {
+export class PercorsiDettagliComponent implements OnInit {
+  @Input() percorso!: Percorsi;
 
+  constructor(
+    private dataTransferService: DataTransferServiceService,
+    private router: Router,
+    private percorsoSrv: PercorsiService
+  ) { }
+
+  ngOnInit() {
+    const href = this.router.url;
+    const idPercorso = href.substring(href.lastIndexOf('/') + 1);
+
+    if (!this.percorso) {
+      this.percorsoSrv.fetchPercorsiByID(idPercorso).subscribe((item: Percorsi) => {
+        this.percorso = item;
+        console.log(this.percorso);
+      });
+    }
+  }
 }
