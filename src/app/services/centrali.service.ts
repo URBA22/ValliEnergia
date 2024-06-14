af225380cdb76a121f16d9d9f1ec04b44ec38ea5
HyperDogeExe <dnltrct@gmail.com>
2024-06-14 12:15:23 +0200

    temporarily fixed scrolling div

--- src/app/services/centrali.service.ts
+++ src/app/services/centrali.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class CentraliService {
 
   constructor(private http:HttpClient) { }
-  private centraliUrl = "http://localhost:3000/centrali"; 
+  private centraliUrl = "http://192.168.123.24:3000/centrali";
 
   fetchCentrali ():Observable<Centrali[]>{
     return this.http.get<Centrali[]>(this.centraliUrl);
