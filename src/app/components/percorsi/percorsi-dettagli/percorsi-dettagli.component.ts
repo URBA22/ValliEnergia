04c4ebaefd7bee8e7871f14ea264a86612adac2f
HyperDogeExe <dnltrct@gmail.com>
2024-06-14 16:38:02 +0200

    added http call on refresh in percorsi detail and fixed some references missing in template

--- src/app/components/percorsi/percorsi-dettagli/percorsi-dettagli.component.ts
+++ src/app/components/percorsi/percorsi-dettagli/percorsi-dettagli.component.ts
@@ -3,30 +3,42 @@ import { Percorsi } from '../../../interfaces/percorsi';
 import { DataTransferServiceService } from '../../../services/data-transfer-service.service';
 import { Router } from '@angular/router';
 import { PercorsiService } from '../../../services/percorsi.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-percorsi-dettagli',
+  standalone: true,
+  imports:[
+    NgFor,
+    NgIf
+  ],
   templateUrl: './percorsi-dettagli.component.html',
   styleUrls: ['./percorsi-dettagli.component.css']
 })
 export class PercorsiDettagliComponent implements OnInit {
-  @Input() percorso!: Percorsi;
+  @Input() percorso: Percorsi;
 
   constructor(
     private dataTransferService: DataTransferServiceService,
     private router: Router,
     private percorsoSrv: PercorsiService
-  ) { }
+  ) { this.percorso = this.getCentrali() }
+
+  getCentrali() : Percorsi
+  {
+    return this.dataTransferService.GetPercorso();
+  }
 
   ngOnInit() {
-    const href = this.router.url;
-    const idPercorso = href.substring(href.lastIndexOf('/') + 1);
+    let href=this.router.url;
+    let idPrc=href.substring( href.lastIndexOf('/')+1,999);
+    if(this.percorso===undefined)
+      {
+        this.percorsoSrv.fetchPercorsiByID(idPrc).subscribe((item:Percorsi)=>{
+          this.percorso=item;
+        });
 
-    if (!this.percorso) {
-      this.percorsoSrv.fetchPercorsiByID(idPercorso).subscribe((item: Percorsi) => {
-        this.percorso = item;
-        console.log(this.percorso);
-      });
-    }
+        return this.percorso;
+      }
   }
 }
