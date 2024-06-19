46c1599cfbf6dcfeebde2a611012a2c55daae1e0
HyperDogeExe <dnltrct@gmail.com>
2024-06-19 12:20:20 +0200

    added function fetchPercorsi for each trail linked to plants

--- src/app/components/centrali/centrale-detail/centrale-detail.component.ts
+++ src/app/components/centrali/centrale-detail/centrale-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Centrali } from '../../../interfaces/centrali';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { DataTransferServiceService } from '../../../services/data-transfer-service.service';
 import { OnInit } from '@angular/core';
 import { window } from 'rxjs';
 import { Router } from '@angular/router';
 import { CentraliService } from '../../../services/centrali.service';
+import { Percorsi } from '../../../interfaces/percorsi';
+import { PercorsiService } from '../../../services/percorsi.service';
+import { PercorsoCardComponent } from '../../percorsi/percorso-card/percorso-card.component';
 
 
 
@@ -16,32 +19,52 @@ import { CentraliService } from '../../../services/centrali.service';
   standalone: true,
   imports: [
     NgFor,
+    CommonModule,
+    PercorsoCardComponent
   ],
 })
 
 
 export class CentraleDetailComponent implements OnInit {
   @Input() centrale: Centrali;
+  linkedPercorsi : Percorsi[] = [];
 
-  constructor(private dataTransferService:DataTransferServiceService , private router:Router,private centraliSrv:CentraliService){ this.centrale = this.getCentrali();}
+  constructor(
+    private dataTransferService:DataTransferServiceService,
+    private router:Router,
+    private centraliSrv:CentraliService,
+    private percorsiSrv : PercorsiService){ this.centrale = this.getCentrali();}
 
   getCentrali() : Centrali
   {
     return this.dataTransferService.GetCentrale();
   }
+
+
   ngOnInit(){
+
     let href=this.router.url;
     let idCentrale=href.substring( href.lastIndexOf('/')+1,999);
-    if(this.centrale===undefined)
+    if(!this.centrale)
       {
         this.centraliSrv.fetchCentraleByID(idCentrale).subscribe((item:Centrali)=>{
           this.centrale=item;
+          this.fetchLinkedPercorsi();
         });
-
         return this.centrale;
       }
-
+      else{
+        this.fetchLinkedPercorsi();
+      }
   }
 
+  private fetchLinkedPercorsi() {
+    if(this.linkedPercorsi.length === 0 && this.centrale?.trails){
+      this.centrale.trails.forEach(trailId => {
+        this.percorsiSrv.fetchPercorsiByID(trailId.toString()).subscribe((item: Percorsi) => {
+          this.linkedPercorsi.push(item);
+        });
+      });
+    }
+  }
 }
-
