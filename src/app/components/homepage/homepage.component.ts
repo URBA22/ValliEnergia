fe61b1769d56438377451e9d1a3edd74ec8579ff
HyperDogeExe <dnltrct@gmail.com>
2024-06-20 16:45:10 +0200

    added homepage basic styling

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
