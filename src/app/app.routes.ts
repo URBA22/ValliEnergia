72eb053b6e869f57d1cc55b337ed8e572f32a7fb
HyperDogeExe <dnltrct@gmail.com>
2024-06-26 15:03:35 +0200

    added button and link to request form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { PercorsiComponent } from './components/percorsi/percorsi.component';
 import { PercorsiDettagliComponent } from './components/percorsi/percorsi-dettagli/percorsi-dettagli.component';
 import { CentraleDetailComponent } from './components/centrali/centrale-detail/centrale-detail.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { RequestformComponent } from './components/requestform/requestform.component';
 
 export const routes: Routes = [
   {path: '', redirectTo:'home', pathMatch: 'full'},
@@ -14,12 +15,16 @@ export const routes: Routes = [
   {path: 'percorsi', component: PercorsiComponent},
   {path: 'centrali/:id', component: CentraleDetailComponent},
   {path: 'percorsi/:id', component: PercorsiDettagliComponent},
+  {path: 'richiedi', component: RequestformComponent},
   {path: 'admin', component: AdminComponent}
 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    NgModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule{}
