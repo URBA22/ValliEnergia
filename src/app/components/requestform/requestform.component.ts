ecbd3bc544ad131e3b6f0cb4fc233c756d80003e
HyperDogeExe <dnltrct@gmail.com>
2024-06-26 15:03:52 +0200

    added request component and added basic styling

---
+++ src/app/components/requestform/requestform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-requestform',
+  standalone: true,
+  imports: [],
+  templateUrl: './requestform.component.html',
+  styleUrl: './requestform.component.css'
+})
+export class RequestformComponent {
+
+}
