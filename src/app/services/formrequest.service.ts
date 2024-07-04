15f436ff7eadfdc7dff6ad74795cc0964188ecc8
HyperDogeExe <dnltrct@gmail.com>
2024-07-04 16:19:19 +0200

    form request fix

--- src/app/services/formrequest.service.ts
+++ src/app/services/formrequest.service.ts
@@ -9,6 +9,7 @@ export class FormrequestService {
   constructor(private http: HttpClient) { }
 
   submitForm(form : any){
+    alert(form);
     const headers = new HttpHeaders().set('Content-type', 'application/json; charset=utf-8');
     let payload = JSON.stringify(form);
     return this.http.post("http://localhost:5000/api/FormReq", payload, {headers: headers}).subscribe(response => alert("Richiesta inviata correttamente"), error => alert("Errore: richiesta non inviata"));
